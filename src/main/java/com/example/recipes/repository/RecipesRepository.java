339933ab71a0701fdcdaf31805e4c46c5be5ffad
Filipe C <filipecavalcantiximenes@gmail.com>
2025-03-13 18:34:38 -0300

    :construction: get recipes by list id

--- src/main/java/com/example/recipes/repository/RecipesRepository.java
+++ src/main/java/com/example/recipes/repository/RecipesRepository.java
@@ -3,7 +3,9 @@ package com.example.recipes.repository;
 import com.example.recipes.domain.recipe.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface RecipesRepository extends JpaRepository<Recipe, UUID> {
+    List<Recipe> findAllByList_Id(UUID listId);
 }
--- src/main/java/com/example/recipes/services/RecipeService.java
+++ src/main/java/com/example/recipes/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.example.recipes.services;
 
+import com.example.recipes.domain.recipesList.RecipesList;
 import com.example.recipes.dto.recipe.RecipeResponseDTO;
 import com.example.recipes.dto.recipesList.RecipesListResponseDTO;
 import com.example.recipes.repository.RecipesRepository;
@@ -7,12 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class RecipeService {
     private final RecipesRepository recipesRepository;
+    private final RecipesListService recipesListService;
 
     public List<RecipeResponseDTO> getAllRecipes() {
         return this.recipesRepository.findAll().stream()
@@ -27,5 +30,23 @@ public class RecipeService {
                 .collect(Collectors.toList());
     }
 
+    public List<RecipeResponseDTO> getRecipesByListId(UUID listId) {
+        try {
+            RecipesListResponseDTO recipesListResponseDTO = recipesListService.getListById(listId);
+            return this.recipesRepository.findAllByList_Id(listId).stream()
+                    .map(recipe -> new RecipeResponseDTO(
+                            recipe.getId(),
+                            recipe.getName(),
+                            recipe.getIngredientesList(),
+                            recipe.getStepsList(),
+                            recipe.getDescription(),
+                            recipe.getRecipesList() != null? recipe.getRecipesList().getId() : null
+                    ))
+                    .collect(Collectors.toList());
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e + "Unable to get recipes by id: " + listId);
+        }
 
+
+    }
 }
